023c181532e76eeed7de4893061594322ff01802
unknown <alete8@gmail.com>
2020-03-03 21:10:07 -0300

    Se modifico la carga de pokemon utilizando promise.all para solucionar el ordenamiento y optimizar la funcion anterior

--- week1-2/src/pokedex.js
+++ week1-2/src/pokedex.js
@@ -1,33 +1,62 @@
-for (i = 1; i <= 150; i++) {
-  fetch('https://pokeapi.co/api/v2/pokemon/' + i)  
-    .then((response) => {
-      return response.json();  
-    })  
-    .then((pokemon) => {
-      const app = document.getElementById('root')
+window.onload = function() {
+  fetchPokemons();
+};
 
-      const card = document.createElement('div')
-      card.setAttribute('class', 'card')
+const fetchPokemons = () => {
+  let urls = [];
 
-      const img = document.createElement('img')
-      img.src = pokemon.sprites.front_default
+  for (var i = 1; i<=150; i++) { 
+    urls.push(`https://pokeapi.co/api/v2/pokemon/${i}`);
+  }
 
-      const h2 = document.createElement('h2')
-      h2.textContent = pokemon.name
+  Promise.all(
+    urls.map(url =>
+      fetch(url)
+        .then(response => response.json())
+        .catch(err => console.error(err))
+    )
+  ).then(pokemons =>
+   pokemons.forEach(pokemon =>   
+   drawPokemon(pokemon)));
+}
 
-      const label = document.createElement('label')
-      label.textContent = "Type: " + pokemon.types.values
+function drawPokemon(pokemon) {
+  
+  const app = document.getElementById('root');
 
-      const button = document.createElement('button')
-      button.textContent = "Add to favs"
+  const card = document.createElement('div');
+  card.setAttribute('class', 'card');
+
+  const img = document.createElement('img');
+  img.src = pokemon.sprites.front_default;
+
+  const h2 = document.createElement('h2');
+  h2.textContent = pokemon.name;
+
+  const label = document.createElement('label');
+  let types = '';
+  
+  pokemon.types.forEach(pokemontype =>
+  types = buildType(pokemontype, types));
+
+  label.textContent = "Type: " + types;
+
+  const button = document.createElement('button');
+  button.textContent = "Add to favs";
       
-      app.appendChild(card)
-      card.appendChild(img)
-      card.appendChild(h2)
-      card.appendChild(label)
-      card.appendChild(button)
+  app.appendChild(card);
+  card.appendChild(img);
+  card.appendChild(h2);
+  card.appendChild(label);
+  card.appendChild(button);
+}
 
-      console.log(pokemon)
-    })  
-    .catch(error => console.error(error))
+function buildType(pokemontype, types) {
+  if (!types) {
+    types = pokemontype.type.name;
+  } else {
+    types = types + ', ' + pokemontype.type.name;
+  }
+
+  return types;
 }
